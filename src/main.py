b6b18eb94271d9a0946ce4907ab22f78d36202c1
Carlos Martínez <josecarlos.martinez@ngahr.com>
2020-02-26 15:59:15 +0100

    Adapt printing result coming from API response format. Improve readability

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from account_counter import AccountCounter
 
 if __name__ == '__main__':
-    ac = AccountCounter(100000)
-    count, nrequests = ac.count_accounts(page_size=2000)
-    print(f"There's {count} accounts in the database. I got it throwing {nrequests} requests")
+    ac = AccountCounter(estimated_max=100000)
+    result = ac.count_accounts(page_size=2000,
+                               verbose=False,
+                               simulate=False,
+                               total_accounts=None)
+    print(f"There's {result['n_accounts']} accounts in the database. I got it throwing {result['requests_used']} requests.")
