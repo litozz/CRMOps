60c428d026ec3faf2b2ae26ccd1e6dcc7f994aa2
Carlos Martínez <josecarlos.martinez@ngahr.com>
2020-02-26 13:06:42 +0100

    Update test according refactorization

--- test_service_simulator.py
+++ test_service_simulator.py
@@ -6,30 +6,30 @@ def account_counter():
     return AccountCounter()
 
 @pytest.mark.parametrize("test_input", [
-	# (total, page_size, page_number)
-	(5, 0,0),
-	(5, -1,2),
-	(5, 1,-2),
-	(5, -1,-2),
-	(5, 1,0),
-	(5, 1,1),
-	(5, 1,2),
-	(5, 1,3),
-	(5, 1,4),
-	(5, 1,5),
-	(5, 2,0),
-	(5, 2,1),
-	(5, 2,2),
-	(5, 2,3),
-	(5, 3,0),
-	(5, 3,1),
+	# (page_size, page_number,total)
+	(0,0,5),
+	(-1,2,5),
+	(1,-2,5),
+	(-1,-2,5),
+	(1,0,5),
+	(1,1,5),
+	(1,2,5),
+	(1,3,5),
+	(1,4,5),
+	(1,5,5),
+	(2,0,5),
+	(2,1,5),
+	(2,2,5),
+	(2,3,5),
+	(3,0,5),
+	(3,1,5),
 ])
 def test_service_simulator(account_counter,test_input):
-	assert account_counter.throw_request( *test_input ) == account_counter.throw_debug_request( *test_input )
+	assert account_counter.throw_request( *test_input ) == account_counter.throw_real_request( *test_input )
 
 
 if __name__ == '__main__':
 	ac = AccountCounter()
-	acinput = (5, -1,0)
+	acinput = (-1,0,5)
 	print(ac.throw_request(*acinput))
-	print(ac.throw_debug_request(*acinput))
+	print(ac.throw_real_request(*acinput))
