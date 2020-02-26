13b93985c8d272b9ebad7540f88fc56ec3ff1dce
Carlos Martínez <josecarlos.martinez@ngahr.com>
2020-02-26 17:39:26 +0100

    Use simulated count on test

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -17,13 +17,13 @@ def account_counter(estimated_max):
 
 @pytest.mark.parametrize("test_input,expected", [
     # (page_size, verbose, simulate, total_accounts)
-    ((5, False, False, 5), 5),
-    ((2000, False, False, 5), 5),
-    ((5, False, False, 2000), 2000),
-    ((2000, False, False, 2000), 2000),
-    ((5, False, False, 0), 0),
-    ((-5, False, False, 5), 'value_error'),
-    ((0, False, False, 5), 'value_error'),
+    ((5, False, True, 5), 5),
+    ((2000, False, True, 5), 5),
+    ((5, False, True, 2000), 2000),
+    ((2000, False, True, 2000), 2000),
+    ((5, False, True, 0), 0),
+    ((-5, False, True, 5), 'value_error'),
+    ((0, False, True, 5), 'value_error'),
 ])
 def test_account_counter(account_counter, test_input, expected):
     count = None
