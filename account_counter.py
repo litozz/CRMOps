d4a3e5cfbe0f1f86b49d4e40c31392772fd9b3d9
Carlos Martínez <josecarlos.martinez@ngahr.com>
2020-02-26 13:06:07 +0100

    Parametrize request to have only a real request function

--- account_counter.py
+++ account_counter.py
@@ -4,7 +4,8 @@ class AccountCounter():
 	def __init__(self):
 		pass
 
-	def throw_request(self, hidden_number_of_docs, page_size, page_number):
+	@staticmethod
+	def throw_request(page_size, page_number, hidden_number_of_docs):
 		if page_size < 0 or page_number < 0:
 			return {"message": "unprocessable entity"}
 
@@ -22,18 +23,10 @@ class AccountCounter():
 		return {str(i):i for i in range(first_account, last_account)}
 
 	@staticmethod
-	def throw_debug_request(total, page_size, page_number):
-		URL = "https://badger-staging-2-pr-2699.herokuapp.com/api/accounts?"
-		PARAMS = {'Total':total, 'pageSize': page_size, 'pageNumber': page_number}
-		r = requests.get(url = URL, params = PARAMS) 
-		data = r.json()
-		return data
-
-
-	@staticmethod
-	def throw_real_request(page_size, page_number):
+	def throw_real_request(page_size, page_number, total=None):
 		URL = "https://badger-staging-2-pr-2699.herokuapp.com/api/accounts?"
 		PARAMS = {'pageSize': page_size, 'pageNumber': page_number}
+		if total:
+			PARAMS['Total']=total
 		r = requests.get(url = URL, params = PARAMS) 
-		data = r.json()
-		return data
+		return r.json()
