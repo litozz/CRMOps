ef123c83c376e89765a7bf24281c46f3371d469c
Carlos Martínez <josecarlos.martinez@ngahr.com>
2020-02-27 10:24:34 +0100

    Adapt test to parameter estimated_max in count_accounts

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -7,24 +7,18 @@ from src.account_counter import AccountCounter
 USE_SIMULATED_SERVICE = True
 
 @pytest.fixture()
-def estimated_max():
-    return 999999
-
-
-@pytest.fixture()
-def account_counter(estimated_max):
-    return AccountCounter(estimated_max)
-
+def account_counter():
+    return AccountCounter()
 
 @pytest.mark.parametrize("test_input,expected", [
-    # (page_size, verbose, simulate, total_accounts)
-    ((5, False, USE_SIMULATED_SERVICE, 5), 5),
-    ((2000, False, USE_SIMULATED_SERVICE, 5), 5),
-    ((5, False, USE_SIMULATED_SERVICE, 2000), 2000),
-    ((2000, False, USE_SIMULATED_SERVICE, 2000), 2000),
-    ((5, False, USE_SIMULATED_SERVICE, 0), 0),
-    ((-5, False, USE_SIMULATED_SERVICE, 5), 'value_error'),
-    ((0, False, USE_SIMULATED_SERVICE, 5), 'value_error'),
+    # (page_size, estimated_max, verbose, simulate, total_accounts)
+    ((5, 99999, False, USE_SIMULATED_SERVICE, 5), 5),
+    ((2000, 99999,  False, USE_SIMULATED_SERVICE, 5), 5),
+    ((5, 99999,  False, USE_SIMULATED_SERVICE, 2000), 2000),
+    ((2000, 99999,  False, USE_SIMULATED_SERVICE, 2000), 2000),
+    ((5, 99999,  False, USE_SIMULATED_SERVICE, 0), 0),
+    ((-5, 99999,  False, USE_SIMULATED_SERVICE, 5), 'value_error'),
+    ((0, 99999,  False, USE_SIMULATED_SERVICE, 5), 'value_error'),
 ])
 def test_account_counter(account_counter, test_input, expected):
     count = None
