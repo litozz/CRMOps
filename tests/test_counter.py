49daa491b8a904e479109962d403310c64ce62fd
Carlos Martínez <josecarlos.martinez@ngahr.com>
2020-02-27 11:18:21 +0100

    Add more test cases to counter test battery

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -19,12 +19,17 @@ def account_counter():
     ((5, 99999,  False, USE_SIMULATED_SERVICE, 0), 0),
     ((-5, 99999,  False, USE_SIMULATED_SERVICE, 5), 'value_error'),
     ((0, 99999,  False, USE_SIMULATED_SERVICE, 5), 'value_error'),
+    ((2000, 99999,  False, USE_SIMULATED_SERVICE, 100000), 'runtime_error'),
+    ((2000, 100000,  False, USE_SIMULATED_SERVICE, 100000), 'runtime_error'),
+    ((2000, 100001,  False, USE_SIMULATED_SERVICE, 100000), 100000),
 ])
 def test_account_counter(account_counter, test_input, expected):
     count = None
     try:
         response = account_counter.count_accounts(*test_input)
         count = response['n_accounts']
-    except ValueError as e:
+    except ValueError:
         count = 'value_error'
+    except RuntimeError:
+        count = 'runtime_error'
     assert count == expected
